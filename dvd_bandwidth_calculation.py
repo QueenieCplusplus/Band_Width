c0d27cf87276e61056725f9f3273db275e8cc750
Queenie Chen <58072875+QueenieCplusplus@users.noreply.github.com>
2020-04-01 09:45:43 +0800

    Add files via upload

--- dvd_bandwidth_calculation.py
+++ dvd_bandwidth_calculation.py
@@ -1,6 +1,6 @@
 # 4/01, am 8:45 - 9:10 (duration: 25 mins)
 # DVD numbers calculation
-# COL : 7 lines
+# COL : 17 lines
 # we define time in sec unit, and speed in Mbps unit.
 
 from datetime import datetime, date
@@ -9,8 +9,8 @@ from datetime import timedelta
 
 # Constant ---->
 DVD_speed = 5 #Mbit/s # MPEG2 Compression
-
 BandWidth_speed = 100 #Mbit/s
+DVD_storage_bytes = 700 # not bit
 
 # month = time.strptime('00:01: 00,000'.split(',')[0],'%H:%M:%S'
 # may_end = date.today() + relativedelta(months=+1)
@@ -24,5 +24,20 @@ month2day = days_passed.days
 day2sec = month2day * 3600 * 24
 print(day2sec) # 2592000
 
-DVD_played_numbers= day2sec * BandWidth_speed / DVD_speed
-print(DVD_played_numbers) #51840000 DVD numbers can be played in this Bandwith for 1 month
+DVD_played_arregate_bits= day2sec * BandWidth_speed / DVD_speed
+print(DVD_played_arregate_bits) #51840000 DVD numbers can be played in this Bandwith for 1 month
+
+def convertor(bytes):
+    bits = bytes / 0.125
+    return bits
+
+DVD_storage_bits= convertor(DVD_storage_bytes)
+print(DVD_storage_bits) # 5600
+
+def dvd_number_checker(in_total_bits):
+    out_num = in_total_bits /DVD_storage_bits
+    return out_num
+
+dvd_consumed_amount = dvd_number_checker(DVD_played_arregate_bits)
+print(dvd_consumed_amount) # 9257
+
